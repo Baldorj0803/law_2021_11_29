c0b1cbdc7366d35cc6735969628f9a35ea4cbca8
baldorj z <baldorj0803@gmail.com>
2022-01-12 16:36:12 +0800

    email damjuulah

--- utils/email.js
+++ utils/email.js
@@ -1,8 +1,12 @@
 const nodemailer = require("nodemailer");
+const fs = require('fs');
+const Hogan = require('hogan.js')
+let template = fs.readFileSync("./views/email.hjs", "utf-8");
+let compiledTemplate = Hogan.compile(template);
 
 // async..await is not allowed in global scope, must use a wrapper
 const sendEmail = async (options) => {
-    //mailTrap
+
     // let transporter = nodemailer.createTransport({
     //     host: "smtp.mailtrap.io",
     //     port: 2525,
@@ -12,10 +16,6 @@ const sendEmail = async (options) => {
     //     }
     // });
 
-    console.log(process.env.SMTP_HOST);
-    console.log(process.env.SMTP_PORT);
-    console.log(process.env.SMTP_USERNAME);
-    console.log(process.env.SMTP_PASSWORD);
     let transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
@@ -27,19 +27,17 @@ const sendEmail = async (options) => {
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "baldorj.z@gobi.mn", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        from: process.env.SMTP_USERNAME, // sender address
+        to: options.to,
+        subject: options.subject,
+        html: compiledTemplate.render(options.message),
     });
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // console.log("Message sent: %s", info.messageId);
 
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    return info;
 }
 
 module.exports = sendEmail;

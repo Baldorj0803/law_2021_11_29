2f777032cadd20c9de0473efb33e8bac56d345d7
baldorj z <baldorj0803@gmail.com>
2022-01-18 09:53:30 +0800

    dashboard get detail zasaw

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -60,22 +60,41 @@ exports.getItemDetail = asyncHandler(async (req, res, next) => {
     // order by i.updatedAt desc
     // limit 10`
 
-    let query = `select i.id, i.createdAt,u.name as userName,u.profession,o.name as orgName,i.name as itemName,
+    // let query = `select i.id, i.createdAt,u.name as userName,u.profession,o.name as orgName,i.name as itemName,
+    // rs.slug,rs.name as statusName,r.*
+    // from items i
+    // left join (
+    // 	select r.itemId,u.profession,u.lastname as rlastname,u.name as ruserName,o.name as rorgName from request  r 
+    // left join recieveUsers ru on r.id=ru.requestId
+    // left join users u on u.id=ru.userId
+    // left join organizations o on u.organizationId=o.id
+    // where r.reqStatusId=${variable.PENDING}
+    // ) as r on r.itemId=i.id
+    // left join users u on i.userId=u.id
+    // left join organizations o on u.organizationId=o.id
+    // left join req_status rs on i.reqStatusId=rs.id
+    // where rs.id not in (${variable.CANCELED},${variable.DRAFT}) 
+    // order by i.updatedAt desc
+    // limit 10;`
+
+    let query = `select i.id, i.createdAt,i.name as itemName,
     rs.slug,rs.name as statusName,r.*
     from items i
     left join (
-		select r.itemId,u.profession,u.lastname as rlastname,u.name as ruserName,o.name as rorgName from request  r 
-    left join recieveUsers ru on r.id=ru.requestId
-    left join users u on u.id=ru.userId
-    left join organizations o on u.organizationId=o.id
-    where r.reqStatusId=${variable.PENDING}
+		select  r.itemId,u.profession,u.lastname as rlastname,u.name as ruserName,
+        o.name as rorgName ,wt.name as wName,count(*) as totalOrg
+        from request  r 
+        left join workflow_templates wt on r.workflowTemplateId=wt.id
+		left join recieveUsers ru on r.id=ru.requestId
+		left join users u on u.id=ru.userId
+		left join organizations o on u.organizationId=o.id
+		where r.reqStatusId=${variable.PENDING}
+		group by r.itemId
     ) as r on r.itemId=i.id
-    left join users u on i.userId=u.id
-    left join organizations o on u.organizationId=o.id
     left join req_status rs on i.reqStatusId=rs.id
-    where rs.id not in (${variable.CANCELED},${variable.DRAFT}) 
+    where rs.id not in (${variable.CANCELED},${variable.DRAFT})  
     order by i.updatedAt desc
-    limit 10;`
+   `
 
     const [uResult, uMeta] = await req.db.sequelize.query(query);
 

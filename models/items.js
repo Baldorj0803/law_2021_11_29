b1b37835458eca4e5c62d715af8664125350fd36
baldorj z <baldorj0803@gmail.com>
2022-01-21 11:00:44 +0800

    -x-

--- models/items.js
+++ models/items.js
@@ -98,10 +98,6 @@ module.exports = function (sequelize, DataTypes) {
       confirmFile: {
         type: DataTypes.STRING,
       },
-      approvedFilePDF: {
-        type: DataTypes.STRING,
-        defaultValue: null,
-      },
       canceledUser: {
         type: DataTypes.BIGINT,
         references: {

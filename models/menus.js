4902a29d2cce8ef4f868a9b04b1b79ae3103c773
baldorj z <baldorj0803@gmail.com>
2022-01-21 10:46:19 +0800

    menu deer icon nemew

--- models/menus.js
+++ models/menus.js
@@ -22,6 +22,10 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING(255),
       allowNull: true,
     },
+    icon: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
     orderNo: {
       type: DataTypes.INTEGER(10).UNSIGNED,
       allowNull: false,
